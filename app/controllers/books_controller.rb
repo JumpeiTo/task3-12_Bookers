2a5621bc6c89bef996e2ae2cc500e5b69c729be7
EC2 Default User <ec2-user@ip-172-31-9-250.ap-northeast-1.compute.internal>
2023-05-07 15:14:34 +0000

    [add]index-show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,17 +2,18 @@ class BooksController < ApplicationController
   
   #一覧　新規投稿画面
   def index
-    @bool = Book.new
+    @books = Book.all
   end
   
   #Create Book(保存)ボタン→詳細画面へ
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to '/books/show'
+    redirect_to book_path(book.id)
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
