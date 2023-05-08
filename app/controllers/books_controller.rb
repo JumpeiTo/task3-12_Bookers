824b6b394a2fc7fe95d079d8218139e4304dac16
EC2 Default User <ec2-user@ip-172-31-9-250.ap-northeast-1.compute.internal>
2023-05-08 08:10:36 +0000

    [add]error-edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,10 +33,13 @@ class BooksController < ApplicationController
   
   # 更新機能
   def update
-    book = Book.find(params[:id])
+    @book = Book.find(params[:id])
     
-     book.update(book_params)
-      redirect_to book_path(book.id)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render:edit
+    end
 
   end
   
